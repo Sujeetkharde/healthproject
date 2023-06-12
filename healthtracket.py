1e6b9bfacd746847e9d2daf65125c8524786cefd
Sujeetkharde <136297303+Sujeetkharde@users.noreply.github.com>
2023-06-12 14:30:32 +0530

    Add files via upload

---
+++ healthtracket.py
@@ -0,0 +1,13 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    notification.notify(
+        title="**Please Drink Water!!",
+        message="Drink water 1L in one hour",
+        app_icon="water.ico.png",
+        timeout=5
+
+
+
+    )
